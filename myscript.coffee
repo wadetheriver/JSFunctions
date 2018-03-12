16e7e2654b1f0fcd637a0e899912892e398b7ae5
Wade Spane <wadetheriver@gmail.com>
2018-03-12 10:18:00 -0700

    First Example of constructor

--- myscript.coffee
+++ myscript.coffee
@@ -1,11 +1,14 @@
-calc =
-  num1 : 3
-  num2 : 4
-  # this will be the object
+Calc = (num1=3, num2=4) ->
+  num1 : num1
+  num2 : num2
   add : (a,b) ->
-    a=this.num1
-    b=this.num2
+    # a=this.num1
+    # b=this.num2
     console.log "this", this
     console.log "numbers", this.num1, this.num2
     console.log "args", arguments
     console.log(this.num1+this.num2)
+
+calc1 = new Calc(76, 24)
+
+console.log calc1.add()
--- myscript.js
+++ myscript.js
@@ -1,15 +1,20 @@
-var calc;
+var Calc, calc1;
 
-calc = {
-  num1: 3,
-  num2: 4,
-  // this will be the object
-  add: function(a, b) {
-    a = this.num1;
-    b = this.num2;
-    console.log("this", this);
-    console.log("numbers", this.num1, this.num2);
-    console.log("args", arguments);
-    return console.log(this.num1 + this.num2);
-  }
+Calc = function(num1 = 3, num2 = 4) {
+  return {
+    num1: num1,
+    num2: num2,
+    add: function(a, b) {
+      // a=this.num1
+      // b=this.num2
+      console.log("this", this);
+      console.log("numbers", this.num1, this.num2);
+      console.log("args", arguments);
+      return console.log(this.num1 + this.num2);
+    }
+  };
 };
+
+calc1 = new Calc(76, 24);
+
+console.log(calc1.add());
