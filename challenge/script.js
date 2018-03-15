9910bd769894fe02661b6e3cf406522152820f00
Wade Spane <wadetheriver@gmail.com>
2018-03-15 16:02:34 -0700

    Completed my solution to social media icon challenge

--- challenge/script.js
+++ challenge/script.js
@@ -4,3 +4,26 @@ var socialMedia = {
   flickr: 'http://flickr.com/planetotheweb',
   youtube: 'http://youtube.com/planetoftheweb'
 };
+
+
+(function(){
+
+  $('.socialmediaicons').each(function(index, value){
+
+      var listHTML = "<ul>";
+        for (key in socialMedia) {
+          var iconName = key + ".png";
+          var iconPath = "images/" + iconName;
+          var theURL = socialMedia[key];
+
+          listHTML += '<li> <a href = "' + theURL + '">';
+          listHTML += '<img src= "' + iconPath + '">'
+          listHTML += '</img><a/>'
+          listHTML += "</li>"
+        }
+
+      listHTML += "</ul>";
+        this.innerHTML = listHTML;
+  });
+
+})();
