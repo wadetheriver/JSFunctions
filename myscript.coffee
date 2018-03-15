13145493f0c737f1db3138ea1aec55b719e75845
Wade Spane <wadetheriver@gmail.com>
2018-03-14 23:18:54 -0700

    call and apply

--- myscript.coffee
+++ myscript.coffee
@@ -1,64 +1,15 @@
-# Return new object from function
-# Adding method to calc prototype will not work,
-# since it is added to constructor function's object prototype and not
-# the returned object's obect prototype
-Calc = (num1=3, num2=4) ->
-  num1 : num1
-  num2 : num2
-  add : (a,b) ->
-    # a=this.num1
-    # b=this.num2
-    console.log "this", this
-    console.log "numbers", this.num1, this.num2
-    console.log "args", arguments
-    console.log(this.num1+this.num2)
-
-calc1 = new Calc(76, 24)
-
-console.log calc1.add()
-
-# A couple functions to be added to prototypes
-getInfo = () ->
-  console.log "getInfo's this", this
-  console.log "The Pets name is #{this.name} and is a #{this.breed}"
-
 speak = (sayWhat) ->
-  console.log "speak's this", this
-  console.log(sayWhat)
+  console.log this
+  console.log this.says
+  console.log sayWhat
 
+barnabas =
+  species: "cat"
+  says:"meow"
 
-# simple constructor and add prototype
-Dog = () ->
-Dog.prototype.getInfo = getInfo
-Dog.prototype.speak = speak
+speak.call(barnabas, barnabas.says)
 
-#instantiate dog with parameters on the fly
-dog1 = new Dog()
-dog1.name = "Ellie"
-dog1.breed = "Redbone"
-dog1.says = "woof"
-dog1.info = () ->
-  console.log("this", this)
-  console.dir("dog1", dog1)
-  console.log "dog1's name is #{dog1.name} and is a #{dog1.breed}"
+speak.apply(barnabas, ["purr"])
 
-# dog1.info()
-dog1.getInfo()
-dog1.speak("woof")
-
-
-
-# use return so coffeescript does not return last statement...
-Cat = (name, breed, says) ->
-  this.name = name
-  this.breed = breed
-  this.says = says
-  return
-
-Cat.prototype.getInfo = getInfo
-Cat.prototype.speak = speak
-
-barnabas = new Cat('Barnabas', 'Orange Tabby', 'meow')
-
-barnabas.getInfo()
-barnabas.speak(barnabas.says)
+meowme = speak.bind(barnabas)
+meowme()
--- myscript.js
+++ myscript.js
@@ -1,79 +1,20 @@
-// Return new object from function
-// Adding method to calc prototype will not work,
-// since it is added to constructor function's object prototype and not
-// the returned object's obect prototype
-var Calc, Cat, Dog, barnabas, calc1, dog1, getInfo, speak;
-
-Calc = function(num1 = 3, num2 = 4) {
-  return {
-    num1: num1,
-    num2: num2,
-    add: function(a, b) {
-      // a=this.num1
-      // b=this.num2
-      console.log("this", this);
-      console.log("numbers", this.num1, this.num2);
-      console.log("args", arguments);
-      return console.log(this.num1 + this.num2);
-    }
-  };
-};
-
-calc1 = new Calc(76, 24);
-
-console.log(calc1.add());
-
-// A couple functions to be added to prototypes
-getInfo = function() {
-  console.log("getInfo's this", this);
-  return console.log(`The Pets name is ${this.name} and is a ${this.breed}`);
-};
+var barnabas, meowme, speak;
 
 speak = function(sayWhat) {
-  console.log("speak's this", this);
+  console.log(this);
+  console.log(this.says);
   return console.log(sayWhat);
 };
 
-// simple constructor and add prototype
-Dog = function() {};
-
-Dog.prototype.getInfo = getInfo;
-
-Dog.prototype.speak = speak;
-
-//instantiate dog with parameters on the fly
-dog1 = new Dog();
-
-dog1.name = "Ellie";
-
-dog1.breed = "Redbone";
-
-dog1.says = "woof";
-
-dog1.info = function() {
-  console.log("this", this);
-  console.dir("dog1", dog1);
-  return console.log(`dog1's name is ${dog1.name} and is a ${dog1.breed}`);
+barnabas = {
+  species: "cat",
+  says: "meow"
 };
 
-// dog1.info()
-dog1.getInfo();
+speak.call(barnabas, barnabas.says);
 
-dog1.speak("woof");
+speak.apply(barnabas, ["purr"]);
 
-// use return so coffeescript does not return last statement...
-Cat = function(name, breed, says) {
-  this.name = name;
-  this.breed = breed;
-  this.says = says;
-};
+meowme = speak.bind(barnabas);
 
-Cat.prototype.getInfo = getInfo;
-
-Cat.prototype.speak = speak;
-
-barnabas = new Cat('Barnabas', 'Orange Tabby', 'meow');
-
-barnabas.getInfo();
-
-barnabas.speak(barnabas.says);
+meowme();
