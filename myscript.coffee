b70122d3bc9c96933bfa2f529e687015ca709d10
Wade Spane <wadetheriver@gmail.com>
2018-03-16 10:23:35 -0700

    Function chaining module methods

--- myscript.coffee
+++ myscript.coffee
@@ -1,12 +1,23 @@
 # module
 # can be invoked anywhere, see index.html
-wade = do () ->
+action = do () ->
   DEFAULTS =
     say: "hello"
+    speed: "fast"
+
 
   speak: () ->
     firstArg = arguments[0] or ''
     statement = firstArg.say or DEFAULTS.say
-    console.log "Wade Says: #{statement}"
+    console.log "Speaking: #{statement}"
+    # 'this' is the outer function object not this one!
+    return this
 
-wade.speak();
+  run: () ->
+    firstArg = arguments[0] or ''
+    running = firstArg.speed or DEFAULTS.speed
+    console.log "Running... #{running}"
+    # 'this' is the outer function object not this one!
+    return this
+
+action.speak({say: "hello"}).run({speed:"slow"});
--- myscript.js
+++ myscript.js
@@ -1,20 +1,35 @@
 // module
 // can be invoked anywhere, see index.html
-var wade;
+var action;
 
-wade = (function() {
+action = (function() {
   var DEFAULTS;
   DEFAULTS = {
-    say: "hello"
+    say: "hello",
+    speed: "fast"
   };
   return {
     speak: function() {
       var firstArg, statement;
       firstArg = arguments[0] || '';
       statement = firstArg.say || DEFAULTS.say;
-      return console.log(`Wade Says: ${statement}`);
+      console.log(`Speaking: ${statement}`);
+      // 'this' is the outer function object not this one!
+      return this;
+    },
+    run: function() {
+      var firstArg, running;
+      firstArg = arguments[0] || '';
+      running = firstArg.speed || DEFAULTS.speed;
+      console.log(`Running... ${running}`);
+      // 'this' is the outer function object not this one!
+      return this;
     }
   };
 })();
 
-wade.speak();
+action.speak({
+  say: "hello"
+}).run({
+  speed: "slow"
+});
