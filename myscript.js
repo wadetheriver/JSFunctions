6921910d5a0190c7c9f58c7b64b7d9d7a45c87a7
Wade Spane <wadetheriver@gmail.com>
2018-03-16 23:38:34 -0700

    Parse local data file and update html with AJAX

--- myscript.js
+++ myscript.js
@@ -1,77 +1,37 @@
-var i, info, info2, key, len, object, output, output2, ref, ref1, ulList, ulList2, value;
-
-info = {
-  "full_name": "Ray Villalobos",
-  "title": "Staff Author",
-  "links": [
-    {
-      "blog": "http://iviewsource.com"
-    },
-    {
-      "facebook": "http://facebook.com/iviewsource"
-    },
-    {
-      "youtube": "http://www.youtube.com/planetoftheweb"
-    },
-    {
-      "podcast": "http://feeds.feedburner.com/authoredcontent"
-    },
-    {
-      "twitter": "http://twitter.com/planetoftheweb"
-    }
-  ]
-};
-
-// for link in info.links
-//   console.log(Object.keys(link) + ":" + Object.values(link))
-output = '';
-
-ref = info.links;
-// go through array
-for (i = 0, len = ref.length; i < len; i++) {
-  object = ref[i];
-// go through each object
-  for (key in object) {
-    value = object[key];
-    if (object.hasOwnProperty(key)) {
-      output += '<li> <a href = "' + value + '">';
-      output += key;
-      output += "</a></li>";
-    }
-  }
+var request;
+if (window.XMLHttpRequest) {
+	request=new XMLHttpRequest();
+} else {
+	request=new ActiveXObject("Microsoft.XMLHTTP");
 }
 
-ulList = document.querySelector("#links");
+// get local file "data.json"
+request.open('GET', 'data.json');
+request.onreadystatechange = function() {
+	if ((request.status === 200) &&
+		(request.readyState === 4)) {
 
-ulList.innerHTML = output;
+      info = JSON.parse(request.responseText);
 
-// objects could be in any order
-info2 = {
-  "full_name": "Ray Villalobos",
-  "title": "Staff Author",
-  "links": {
-    "blog": "http://iviewsource.com",
-    "facebook": "http://facebook.com/iviewsource",
-    "youtube": "http://www.youtube.com/planetoftheweb",
-    "podcast": "http://feeds.feedburner.com/authoredcontent",
-    "twitter": "http://twitter.com/planetoftheweb"
-  }
-};
+			var output='';
+      // got through array
+			for (var i = 0; i <= info.links.length-1; i++) {
+        object=info.links[i];
+				for (key in object) {
+          // go through each object
+					if (object.hasOwnProperty(key)) {
+						output += '<li>' +
+						'<a href = "' + object[key] +
+						'">' + key + '</a>';
+						'</li>';
+			    }
+				}
+			}
 
-// Put links in index.html list
-output2 = "";
+			var update = document.getElementById('links');
+			update.innerHTML = output;
 
-ref1 = info2.links;
-for (key in ref1) {
-  value = ref1[key];
-  // console.log key + " " + value
-  if (info2.links.hasOwnProperty(key)) {
-    output2 += '<li> <a href = "' + value + '">';
-    output2 += key;
-    output2 += "</a></li>";
-  }
-}
 
-ulList2 = document.querySelector("#links2");
-
-ulList2.innerHTML = output2;
+	} //ready
+} //event
+request.send();
