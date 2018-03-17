c40e3cb623da10ac47cb0897fcb019a80976d284
Wade Spane <wadetheriver@gmail.com>
2018-03-16 22:55:33 -0700

    iterate objects and arrays of objects

--- myscript.coffee
+++ myscript.coffee
@@ -9,11 +9,45 @@ info =
     { "twitter"  : "http://twitter.com/planetoftheweb" }
   ]
 
+# for link in info.links
+#   console.log(Object.keys(link) + ":" + Object.values(link))
 
-# for...in is "of" in CS
-for key, value of info
-  console.log key + " " + value
+output = ''
 
-  if key is "links"
-    for link in info.links
-      console.log(Object.keys(link) + ":" + Object.values(link))
+# go through array
+for object in info.links
+  # go through each object
+  for key, value of object
+    if object.hasOwnProperty(key)
+      output += '<li> <a href = "' + value + '">'
+      output += key
+      output += "</a></li>"
+
+ulList = document.querySelector("#links")
+ulList.innerHTML = output
+
+
+# objects could be in any order
+info2 =
+  "full_name" : "Ray Villalobos"
+  "title" : "Staff Author"
+  "links" :
+    "blog" : "http://iviewsource.com"
+    "facebook" : "http://facebook.com/iviewsource"
+    "youtube"  : "http://www.youtube.com/planetoftheweb"
+    "podcast"  : "http://feeds.feedburner.com/authoredcontent"
+    "twitter"  : "http://twitter.com/planetoftheweb"
+
+
+# Put links in index.html list
+output2 = ""
+
+for key, value of info2.links
+  # console.log key + " " + value
+  if info2.links.hasOwnProperty(key)
+    output2 += '<li> <a href = "' + value + '">'
+    output2 += key
+    output2 += "</a></li>"
+
+ulList2 = document.querySelector("#links2")
+ulList2.innerHTML = output2
--- myscript.js
+++ myscript.js
@@ -1,4 +1,4 @@
-var i, info, key, len, link, ref, value;
+var i, info, info2, key, len, object, output, output2, ref, ref1, ulList, ulList2, value;
 
 info = {
   "full_name": "Ray Villalobos",
@@ -22,15 +22,56 @@ info = {
   ]
 };
 
-// for...in is "of" in CS
-for (key in info) {
-  value = info[key];
-  console.log(key + " " + value);
-  if (key === "links") {
-    ref = info.links;
-    for (i = 0, len = ref.length; i < len; i++) {
-      link = ref[i];
-      console.log(Object.keys(link) + ":" + Object.values(link));
+// for link in info.links
+//   console.log(Object.keys(link) + ":" + Object.values(link))
+output = '';
+
+ref = info.links;
+// go through array
+for (i = 0, len = ref.length; i < len; i++) {
+  object = ref[i];
+// go through each object
+  for (key in object) {
+    value = object[key];
+    if (object.hasOwnProperty(key)) {
+      output += '<li> <a href = "' + value + '">';
+      output += key;
+      output += "</a></li>";
     }
   }
 }
+
+ulList = document.querySelector("#links");
+
+ulList.innerHTML = output;
+
+// objects could be in any order
+info2 = {
+  "full_name": "Ray Villalobos",
+  "title": "Staff Author",
+  "links": {
+    "blog": "http://iviewsource.com",
+    "facebook": "http://facebook.com/iviewsource",
+    "youtube": "http://www.youtube.com/planetoftheweb",
+    "podcast": "http://feeds.feedburner.com/authoredcontent",
+    "twitter": "http://twitter.com/planetoftheweb"
+  }
+};
+
+// Put links in index.html list
+output2 = "";
+
+ref1 = info2.links;
+for (key in ref1) {
+  value = ref1[key];
+  // console.log key + " " + value
+  if (info2.links.hasOwnProperty(key)) {
+    output2 += '<li> <a href = "' + value + '">';
+    output2 += key;
+    output2 += "</a></li>";
+  }
+}
+
+ulList2 = document.querySelector("#links2");
+
+ulList2.innerHTML = output2;
