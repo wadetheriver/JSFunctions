c5a7b58b41225cddcb03d260504c988e2a5fbadc
Wade Spane <wadetheriver@gmail.com>
2018-03-17 06:51:16 -0700

    simple jsonp implementation. Data file on mamp called and parsed from atom live server

--- myscript.js
+++ myscript.js
@@ -1,37 +1,17 @@
-var request;
-if (window.XMLHttpRequest) {
-	request=new XMLHttpRequest();
-} else {
-	request=new ActiveXObject("Microsoft.XMLHTTP");
+function dataHandler(info) {
+	var output='';
+	for (var i = 0; i <= info.links.length-1; i++) {
+		for (key in info.links[i]) {
+			if (info.links[i].hasOwnProperty(key)) {
+				output += '<li>' +
+				'<a href = "' + info.links[i][key] +
+				'">' + key + '</a>';
+				'</li>';
+	    }
+		}
+	}
+
+	var update = document.getElementById('links');
+	update.innerHTML = output;
+
 }
-
-// get local file "data.json"
-request.open('GET', 'data.json');
-request.onreadystatechange = function() {
-	if ((request.status === 200) &&
-		(request.readyState === 4)) {
-
-      info = JSON.parse(request.responseText);
-
-			var output='';
-      // got through array
-			for (var i = 0; i <= info.links.length-1; i++) {
-        object=info.links[i];
-				for (key in object) {
-          // go through each object
-					if (object.hasOwnProperty(key)) {
-						output += '<li>' +
-						'<a href = "' + object[key] +
-						'">' + key + '</a>';
-						'</li>';
-			    }
-				}
-			}
-
-			var update = document.getElementById('links');
-			update.innerHTML = output;
-
-
-	} //ready
-} //event
-request.send();
